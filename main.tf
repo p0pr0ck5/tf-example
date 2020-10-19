1ad0b4130e9d6b8c36efb92cb62519817e71acdb
Robert Paprocki <robert@cryptobells.com>
2020-10-19 10:49:20 -0700

    meta yo

--- main.tf
+++ main.tf
@@ -9,6 +9,13 @@ terraform {
 resource "random_pet" "pet" {}
 resource "random_pet" "other_pet" {}
 
+data "http" "example" {
+  url = "http://169.254.169.254/latest/meta-data/local-ipv4"
+}
+output "http" {
+  value = data.http.example.body
+}
+
 output "pet" {
   value = random_pet.pet.id
 }
