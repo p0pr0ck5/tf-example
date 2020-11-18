7cc5bdbd22a1abd8cc9f0b8cb6913f762667ba62
Robert Paprocki <robert@cryptobells.com>
2020-11-18 13:27:25 -0800

    custom provider test

--- main.tf
+++ main.tf
@@ -1,20 +1,21 @@
 terraform {
-  required_version = ">= 0.12.26"
+  required_version = ">= 0.13.5"
 
   required_providers {
-    random = "~> 2.3"
+    random = {
+      source = "hashicorp/random"
+      version = "~> 2.3"
+    }
+
+    yaml = {
+      version = "2.1"
+      source = "providers.local/myorg/yaml"
+    }
   }
 }
 
 resource "random_pet" "pet" {}
 resource "random_pet" "other_pet" {}
-
-data "http" "example" {
-  url = "http://169.254.169.254/latest/meta-data/local-ipv4"
-}
-output "http" {
-  value = data.http.example.body
-}
 
 output "pet" {
   value = random_pet.pet.id
@@ -22,4 +23,25 @@ output "pet" {
 
 output "other_pet" {
   value = random_pet.other_pet.id
+}
+
+data "yaml_to_json" "doc" {
+  input = <<EOF
+foo: 123
+456: bar
+columns:
+  - name: column0
+    type: boolean
+  - name: column1
+    type: integer
+EOF
+
+}
+
+output "json" {
+  value = data.yaml_to_json.doc.output
+}
+
+output "result" {
+  value = jsondecode(data.yaml_to_json.doc.output)
 }
