16fe921414af50418d506073ed1a4db4a9fdaeaa
Robert Paprocki <robert@cryptobells.com>
2021-01-22 09:59:40 -0800

    random only

--- main.tf
+++ main.tf
@@ -6,11 +6,6 @@ terraform {
       source = "hashicorp/random"
       version = "~> 2.3"
     }
-
-    yaml = {
-      version = "2.1"
-      source = "providers.local/myorg/yaml"
-    }
   }
 }
 
@@ -23,25 +18,4 @@ output "pet" {
 
 output "other_pet" {
   value = random_pet.other_pet.id
-}
-
-data "yaml_to_json" "doc" {
-  input = <<EOF
-foo: 123
-456: bar
-columns:
-  - name: column0
-    type: boolean
-  - name: column1
-    type: integer
-EOF
-
-}
-
-output "json" {
-  value = data.yaml_to_json.doc.output
-}
-
-output "result" {
-  value = jsondecode(data.yaml_to_json.doc.output)
 }
