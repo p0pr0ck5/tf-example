32013d4249f0eb328e658422ae2ce977298c3837
Robert Paprocki <robert@cryptobells.com>
2021-03-25 12:49:03 -0700

    add y

---
+++ y/main.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_version = ">= 0.13.5"
+
+  required_providers {
+    yaml = {
+      version = "2.1"
+      source = "providers.local/myorg/yaml"
+    }
+  }
+}
+
+data "yaml_to_json" "doc" {
+  input = <<EOF
+foo: 123
+456: bar
+columns:
+  - name: column0
+    type: boolean
+  - name: column1
+    type: integer
+EOF
+
+}
+
+output "json" {
+  value = data.yaml_to_json.doc.output
+}
+
+output "result" {
+  value = jsondecode(data.yaml_to_json.doc.output)
+}
