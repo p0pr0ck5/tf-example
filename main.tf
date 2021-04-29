006026c1326839aca3333e36b3d0d8616a928c42
Robert Paprocki <robert@cryptobells.com>
2021-04-29 14:44:16 -0700

    listener

--- main.tf
+++ main.tf
@@ -28,6 +28,12 @@ data "aws_ami" "ubuntu" {
 resource "aws_elb" "e" {
   name               = "foobar-terraform-elb"
   availability_zones = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  listener {
+    instance_port     = 8000
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
+  }
 }
 
 resource "random_pet" "pet" {}
