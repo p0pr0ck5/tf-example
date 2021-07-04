f899150efb0cd0e1768912a14c354ccfea88d171
Robert Paprocki <robert@cryptobells.com>
2021-07-03 20:18:13 -0700

    MOAR

--- stress.tf
+++ stress.tf
@@ -1,8 +1,8 @@
 resource "stress_cpu" "why-are-you-doing-this" {
-  duration = 300 # How long to keep the load up in Seconds
+  duration = 330 # How long to keep the load up in Seconds
 }
 
 resource "stress_memory" "sad-ram-dot-emoji" {
-  duration = 300 # How long to hold the memory
-  size = 256 # Memory to allocate in MB
+  duration = 330 # How long to hold the memory
+  size = 512 # Memory to allocate in MB
 }
