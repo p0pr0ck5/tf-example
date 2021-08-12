0a20641c07a702a51ead46560c60bc96ec197c45
Robert Paprocki <robert@cryptobells.com>
2021-08-12 09:00:33 -0700

    update

--- stress.tf
+++ stress.tf
@@ -1,8 +1,8 @@
 resource "stress_cpu" "why-are-you-doing-this" {
-  duration = 331 # How long to keep the load up in Seconds
+  duration = 330 # How long to keep the load up in Seconds
 }
 
 resource "stress_memory" "sad-ram-dot-emoji" {
   duration = 330 # How long to hold the memory
-  size = 256 # Memory to allocate in MB
+  size = 255 # Memory to allocate in MB
 }
