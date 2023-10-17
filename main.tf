e0a09fa28e7fa69775eca47c33a38e698d56418f
Robert Paprocki <robert@cryptobells.com>
2023-10-17 11:57:16 -0700

    no data

--- main.tf
+++ main.tf
@@ -9,21 +9,6 @@ terraform {
   }
 }
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
 
 resource "aws_elb" "e" {
   name               = "foobar-terraform-elb"
